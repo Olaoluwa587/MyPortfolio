a1d756ac58915e466041714f07f8e2903d238c16
ooluwa587 <ooluwa587@gmail.com>
2025-03-24 13:26:26 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Full Stack Developer.', 'A Law student at York University.', 'Entreprenuer'],
+    typeSpeed: 80,
+    backSpeed:80,
+    backDelay:1200,
+    loop:true,
+  });
